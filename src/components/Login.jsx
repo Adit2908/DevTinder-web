4d3ca588051186aec1ef47bdcedc65ad9eb007cd
Adit2908 <Adit2908>
2026-08-19 17:38:44 +0530

    try

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
         },
         { withCredentials: true },
       );
-      dispatch(addUser({ ...res.data.data, token: res.data.token }));
+      dispatch(addUser(res.data));
       return navigate("/");
     } catch (err) {
       setError(err?.response?.data || "Something went wrong");
