0fd61b67457bfe2c52afc5e6560dd754f5c675e2
Adit2908 <Adit2908>
2026-08-21 00:42:24 +0530

    fix: Feed was sending Bearer token header instead of cookies; clean up dead code

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,94 +1,3 @@
-// // import React, { useEffect } from "react";
-// // import NavBar from "./NavBar";
-// // import { Outlet, useNavigate } from "react-router";
-// // import Footer from "./Footer";
-// // import axios from "axios"
-// // import { BASE_URL } from "../utils/constants";
-// // import { addUser } from "../utils/userSlice";
-// // import { useDispatch, useSelector } from "react-redux";
-
-// // const Body = () => {
-// //   const dispatch = useDispatch();
-// //   const navigate = useNavigate();
-// //   const userData = useSelector((store) => store.user);
-
-// //   const fetchUser = async () => {
-// //     if(userData) return;
-// //     try {
-// //       const res = await axios.get(BASE_URL + "/profile/view", {
-// //         withCredentials: true,
-// //       });
-// //       dispatch(addUser(res.data));
-// //     } catch (err) {
-// //       if (err.response?.status === 401) {
-// //         navigate("/login");
-// //       }
-
-// //       console.error(err);
-// //     }
-// //   };
-
-// //   useEffect(() => {
-// //     fetchUser();
-// //   }, []);
-
-// //   return (
-// //     <div>
-// //       <NavBar />
-// //       <Outlet />
-// //       <Footer />
-// //     </div>
-// //   );
-// // };
-
-// // export default Body;
-
-
-// import React, { useEffect } from "react";
-// import NavBar from "./NavBar";
-// import { Outlet, useNavigate } from "react-router";
-// import Footer from "./Footer";
-// import axios from "axios";
-// import { BASE_URL } from "../utils/constants";
-// import { addUser } from "../utils/userSlice";
-// import { useDispatch, useSelector } from "react-redux";
-
-// const Body = () => {
-//   const dispatch = useDispatch();
-//   const navigate = useNavigate();
-//   const userData = useSelector((store) => store.user);
-
-//   const fetchUser = async () => {
-//     if (userData) return;
-//     try {
-//       const res = await axios.get(BASE_URL + "/profile/view", {
-//         headers: { Authorization: `Bearer ${userData?.token}` },
-//       });
-//       dispatch(addUser({ ...res.data.data, token: userData?.token }));
-//     } catch (err) {
-//       if (err.response?.status === 401) {
-//         navigate("/login");
-//       }
-//       console.error(err);
-//     }
-//   };
-
-//   useEffect(() => {
-//     fetchUser();
-//   }, []);
-
-//   return (
-//     <div>
-//       <NavBar />
-//       <Outlet />
-//       <Footer />
-//     </div>
-//   );
-// };
-
-// export default Body;
-
-
 import React, { useEffect } from "react";
 import NavBar from "./NavBar";
 import { Outlet, useNavigate } from "react-router";
@@ -110,9 +19,7 @@ const Body = () => {
       const res = await axios.get(BASE_URL + "/profile/view", {
         withCredentials: true,
       });
-
-      // Depending on whether your backend returns res.data or res.data.data
-      dispatch(addUser(res.data.data || res.data));
+      dispatch(addUser(res.data));
     } catch (err) {
       if (err.response?.status === 401) {
         navigate("/login");
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,41 +1,3 @@
-// import axios from "axios"
-// import React, { useEffect } from "react";
-// import { BASE_URL } from "../utils/constants";
-// import { useDispatch, useSelector } from "react-redux";
-// import { addFeed } from "../utils/feedSlice";
-// import UserCard from "./UserCard";
-
-// const Feed = () => {
-//   const feed = useSelector((store) => store.feed);
-//   const dispatch = useDispatch();
-
-//   const getFeed = async () => {
-//     if (feed) return;
-//     try {
-//       const res = await axios.get(BASE_URL + "/feed",{withCredentials:true});
-//       dispatch(addFeed(res.data));
-//     } catch (err) {
-//       console.error(err);
-//     }
-//   };
-
-//   useEffect(()=>{
-//     getFeed();
-//   },[]) 
-  
-//   if(!feed) return;
-
-//   if(feed.length <=0) return <h1 className="flex justify-center my-10">No new users founds!</h1>
-
-
-//   return (
-//     feed && (
-//   <div className="flex justify-center my-10"><UserCard user={feed[0]}/></div>)
-//   );
-// };
-
-// export default Feed;
-
 import axios from "axios";
 import React, { useEffect } from "react";
 import { BASE_URL } from "../utils/constants";
@@ -45,14 +7,13 @@ import UserCard from "./UserCard";
 
 const Feed = () => {
   const feed = useSelector((store) => store.feed);
-  const user = useSelector((store) => store.user);
   const dispatch = useDispatch();
 
   const getFeed = async () => {
     if (feed) return;
     try {
       const res = await axios.get(BASE_URL + "/feed", {
-        headers: { Authorization: `Bearer ${user?.token}` },
+        withCredentials: true,
       });
       dispatch(addFeed(res.data));
     } catch (err) {
@@ -67,7 +28,7 @@ const Feed = () => {
   if (!feed) return;
 
   if (feed.length <= 0)
-    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
+    return <h1 className="flex justify-center my-10">No new users found!</h1>;
 
   return (
     feed && (
