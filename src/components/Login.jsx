95dc5d89bbb434b9d4900b25d416b2a3943d1428
Adit2908 <Adit2908>
2026-08-23 18:20:37 +0530

    commited some changes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
     try{
       const res=await axios.post(BASE_URL + "/signup", {firstName,lastName,emailId,password}, {withCredentials:true});
       console.log(res.data)
-      dispatch(addUser({ ...res.data.data, token: res.data.token }));
+      dispatch(addUser(res.data.data));
       return navigate("/profile");
       
     }catch(err){
